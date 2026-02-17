80f6b52cfd3789c6e23362ea5a94ab6790eaa1aa
sewminiWDST <sewmini-ps21035@stu.kln.ac.lk>
2026-02-17 23:15:18 +0530

    Update InquiryRepository.java

--- src/main/java/com/highwaylink/repository/InquiryRepository.java
+++ src/main/java/com/highwaylink/repository/InquiryRepository.java
@@ -5,6 +5,16 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.highwaylink.model.Inquiry;
+/**
+ * InquiryRepository
+ *
+ * Repository interface for Inquiry collection.
+ * Extends MongoRepository to provide:
+ * - Basic CRUD operations
+ * - Pagination and sorting support
+ *
+ * MongoDB Collection: inquiries
+ */
 
 public interface InquiryRepository extends MongoRepository<Inquiry, String> {
     List<Inquiry> findByUserEmail(String userEmail);
